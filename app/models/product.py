f80a94a7ed738934e2b5764e940c9a85df1e641d
MichaelMarch <s222074@student.uek.krakow.pl>
2022-06-10 08:26:32 +0200

    HOMEWORK MM

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,6 @@
 class Product:
-
-    def __init__(self):
-        pass
+    def __init__(self, product_id, stats, opinion):
+        self.product_id = product_id
+        self.stats = stats
+        self.opinion = opinion
+    
